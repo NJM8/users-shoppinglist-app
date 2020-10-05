8679edaaaec57aa1ad5fbd7ad10da72f2590ca9e
natemallison <healthynate@gmail.com>
2020-10-05 09:41:48 -0400

    Mongoose updates.

--- models/index.js
+++ models/index.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 mongoose.set("debug", true);
+mongoose.set("useUnifiedTopology", true);
 mongoose.Promise = global.Promise;
 
 mongoose
-  .connect(process.env.DB_URI || "mongodb://localhost/users-shoppinglist-app")
+  .connect(process.env.DB_URI || "mongodb://localhost/users-shoppinglist-app", {
+    useNewUrlParser: true,
+  })
   .then(() => {
     return console.log("Connected to MongoDB");
   })
